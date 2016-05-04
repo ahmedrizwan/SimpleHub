307173b509f237d727b81393a3f749b64a40d5ab
Ahmed Rizwan <ahmedrizwan@outlook.com>
2016-05-04 07:44:21 +0400

    Hide initial views

--- SimpleHub/ViewController.swift
+++ SimpleHub/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var editTextUserName: UITextField!
+    @IBOutlet weak var buttonSearch: UIButton!
+    @IBOutlet weak var imageViewUser: UIImageView!
+    @IBOutlet weak var labelUserName: UILabel!
+    @IBOutlet weak var labelTotalRepos: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        imageViewUser.hidden = true
+        labelTotalRepos.hidden = true
+        labelUserName.hidden = true
     }
 
 
