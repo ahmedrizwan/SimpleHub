a020fc6d8f53085067894373bce18e8a952d0e8c
Ahmed Rizwan <ahmedrizwan@outlook.com>
2016-05-04 07:16:06 +0400

    Initial Commit

---
+++ SimpleHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleHub
+//
+//  Created by Ahmed Rizwan on 04/05/2016.
+//  Copyright © 2016 Ahmed Rizwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
